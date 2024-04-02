87b6b0933aa5c63bb3899b417fb355320532bd95
Yusra <yusra7003@gmail.com>
2024-04-02 22:12:59 +0500

    my changes

--- index.js
+++ index.js
@@ -3,20 +3,47 @@ import inquirer from "inquirer";
 let todos = [];
 let condition = true;
 while (condition) {
-    let addTask = await inquirer.prompt([
+    let operations = await inquirer.prompt([
         {
-            name: "todo",
-            type: "input",
-            message: "What do you want to add in your todos?",
-        },
-        {
-            name: "addMore",
-            type: "confirm",
-            message: "Do you want to add more?",
-            default: "false",
+            name: "todoslist",
+            type: "list",
+            message: "Select one",
+            choices: ["Add task", "View tasks", "Remove task"],
         }
     ]);
-    todos.push(addTask.todo);
-    condition = addTask.addMore;
-    console.log(todos);
+    if (operations.todoslist === "Add task") {
+        let Addtask = await inquirer.prompt([
+            {
+                name: "todo",
+                type: "input",
+                message: "What do you want to add in your todos?",
+            },
+            {
+                name: "addMore",
+                type: "confirm",
+                message: "Do you want to add more?",
+                default: false,
+            }
+        ]);
+        todos.push(Addtask.todo);
+        condition = Addtask.addMore;
+        console.log(todos);
+    }
+    else if (operations.todoslist === "View tasks") {
+        console.log(todos);
+    }
+    else if (operations.todoslist === "Remove task") {
+        let removetask = await inquirer.prompt([
+            {
+                name: "todo",
+                type: "list",
+                message: "Select task to remove",
+                choices: todos.map((item) => item),
+            }
+        ]);
+        let newtodos = todos.filter((val) => val !== removetask.todo);
+        todos = [...newtodos];
+        console.log(todos);
+    }
 }
+;
